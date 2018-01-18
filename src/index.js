843c977ff739a2c9826f498a45aed4fdbdc7a488
DizNicolasAmor <diz.nicolasamor@gmail.com>
2018-01-18 09:42:17 -0300

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/css/index.css';
-import App from './assets/js/App';
-import registerServiceWorker from './assets/js/registerServiceWorker';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
