38885086aeec418873b8ece21eb25020bd7e9293
Nicolas Diz <diz.nicolasamor@gmail.com>
2021-05-24 10:29:48 -0300

    MASTER: refactor Header.js

--- src/assets/js/Header.js
+++ src/assets/js/Header.js
@@ -1,16 +1,14 @@
 import React from 'react';
 
-
-const Header = () => {
-  return (
-    <div id="header">
-      	<h3 className="title" id="tienda-de">Tienda de</h3>
-        <h1 className="title" id="pianos-electricos">Pianos eléctricos</h1>
-        <h1><hr className="center-block"/></h1>
-        <h1 className="title" id="casa-ramirez">Casa Ramírez</h1>
-        <h1><hr className="center-block"/></h1>
-    </div>
-  );
-} 
+const Header = () => (
+	<div id="header">
+		<h1 className="title">
+			Tienda de Pianos eléctricos
+			<br />
+			<br />
+			Casa Ramírez
+		</h1>
+  </div>
+);
 
 export default Header;
